22a5ed6616bfdd18eb6d5a8beb73ab0d18e50fbc
shahidbhatt07 <bhatshahid6542@gmail.com>
2022-11-07 22:07:53 +0530

    :heart: Global execution context

---
+++ 01.js
@@ -0,0 +1,15 @@
+//Global execution context
+console.log(this);
+console.log(window);
+console.log(firstName);
+var firstName = "shahid";
+console.log(firstName);
+
+
+//output
+/*Window
+ Window
+undefined
+shahid */
+
+//for better understanding, refer to notebook
