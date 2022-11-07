24e88d4a04ffc9fe70dcd8bbbc2b430772d20a66
shahidbhatt07 <bhatshahid6542@gmail.com>
2022-11-07 22:19:03 +0530

    :heart: GEC and FEC

--- 01.js
+++ 01.js
@@ -1,9 +1,9 @@
 //Global execution context
-console.log(this);
-console.log(window);
-console.log(firstName);
-var firstName = "shahid";
-console.log(firstName);
+// console.log(this);
+// console.log(window);
+// console.log(firstName);
+// var firstName = "shahid";
+// console.log(firstName);
 
 
 //output
@@ -11,5 +11,27 @@ console.log(firstName);
  Window
 undefined
 shahid */
+
+//for better understanding, refer to notebook
+
+
+//function execution context
+let foo = "foo";
+console.log(foo);
+function getFullName(firstName, lastName) {
+    console.log(arguments);
+    let myVar = "var inside function";
+    console.log(myVar);
+    const fullName = firstName + " " + lastName;
+    return fullName;
+}
+const personName = getFullName("shahid", "bhat");
+console.log(personName);
+
+/* output:
+foo
+Arguments(2) ['shahid', 'bhat', callee: ƒ, Symbol(Symbol.iterator): ƒ]
+var inside function
+shahid bhat */
 
 //for better understanding, refer to notebook
