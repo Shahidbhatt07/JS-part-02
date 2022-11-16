271d754da1cf39deed730a618a1539af9a845ffc
shahidbhatt07 <bhatshahid6542@gmail.com>
2022-11-16 14:36:02 +0530

    :rocket: innerHTML

---
+++ DOM-Tutorial/07.js
@@ -0,0 +1,12 @@
+//innerHTML
+const headline = document.querySelector(".headline");
+console.log(headline.innerHTML);
+
+//how to change innerHTML 
+// headline.innerHTML = "<h1>innerHTML changed</h1>";
+
+//how to add other things
+// headline.innerHTML += "<button class = \"btn\"> Learn More </button>";
+
+console.log(headline.innerHTML);
+
