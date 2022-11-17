f2fd2bf3071e5a139ae0a55d902e0293992737c2
shahidbhatt07 <bhatshahid6542@gmail.com>
2022-11-17 18:54:37 +0530

    :triangular_flag_on_post: classList

---
+++ DOM-Tutorial/09.js
@@ -0,0 +1,18 @@
+//classList -> it tells us about how many classes are present in a particular section
+const sectionTodo = document.querySelector(".section-todo");
+console.log(sectionTodo.classList); //DOMTokenList(2) ['section-todo', 'container', value: 'section-todo container']
+
+// how to add new class
+// sectionTodo.classList.add("bg-dark");
+
+//how to remove a class which is present there
+// sectionTodo.classList.remove("container")
+
+//contains -> its tells us whether the class is present or not (true/false)
+// sectionTodo.classList.add("bg-dark");
+const bgDark = sectionTodo.classList.contains("bg-dark")
+console.log(bgDark); //false
+
+//toggle -> it will add a new class if that class is not already present there or it will remove that class if already present
+// sectionTodo.classList.add("bg-dark");
+// sectionTodo.classList.toggle("bg-dark"); 
